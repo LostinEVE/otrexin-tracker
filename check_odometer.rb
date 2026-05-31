270649ef10f07a9e4f649a1ad6cbe58f1a73d461
LostinEVE <jrogers4174@outlook.com>
2026-05-31 18:35:27 -0400

    Remove test file

--- check_odometer.rb
+++
@@ -1,6 +0,0 @@
-require_relative 'config/environment'
-
-user = User.first
-user.fuel_logs.where('odometer > 100000').order(odometer: :desc).each do |log|
-  puts "Truck: #{log.truck&.display_name}, Date: #{log.created_at.to_date}, Odometer: #{log.odometer}"
-end
